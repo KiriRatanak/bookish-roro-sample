506966463bc1975847e303bb61eaa470478a6aad
Kiriratanak Vorn <sir.ratanak@gmail.com>
2020-11-28 13:26:52 +0700

    integrate docker

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ const app = express()
 app.use(bodyParser.json())
 
 
-app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT}`))
+app.listen(process.env.PORT || 5000, () => console.log(`Listening on port 5000`))
